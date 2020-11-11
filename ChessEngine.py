13cd5dec2a644bb440ba0c2a0f3366ee1f0b3d8b
fredrikmejs <s185130@student.dtu.dk>
2020-11-11 19:52:09 +0100

    pawn moves works

--- ChessEngine.py
+++ ChessEngine.py
@@ -1,32 +1,87 @@
-class GameState():
+class GameState:
     def __init__(self):
-        #Creates the board
+        # Creates the board
         self.board = [
-        ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
-        ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
-        ["--", "--", "--", "--", "--", "--", "--", "--"],
-        ["--", "--", "--", "--", "--", "--", "--", "--"],
-        ["--", "--", "--", "--", "--", "--", "--", "--"],
-        ["--", "--", "--", "--", "--", "--", "--", "--"],
-        ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
-        ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]]
 
-        #White always start
+        # White always start
         self.whiteToMove = True
         self.moveLog = []
 
-    def makeMove(self,move):
-        a = move.pieceMoved
-        b = self.board[move.startRow][move.startCol]
-        c = self.board[move.endRow][move.endCol]
-
-        self.board[move.startRow][move.startCol] = "--" #Set the moved from postion empty
-        self.board[move.endRow][move.endCol] = move.pieceMoved #move the brick to the given position
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = "--"  # Set the moved from postion empty
+        self.board[move.endRow][move.endCol] = move.pieceMoved  # move the brick to the given position
         self.moveLog.append(move)
-        self.whiteToMove = not self.whiteToMove #Switches turn.
+        self.whiteToMove = not self.whiteToMove  # Switches turn.
+
+        if self.whiteToMove:
+            print("White to move")
+        else:
+            print("Black to move")
+
+    def getValidMoves(self):
+        return self.getAllPossibleMoves()
+
+    def getAllPossibleMoves(self):
+        moves = []
+        for r in range(len(self.board)):  # number of rows
+            for c in range(len(self.board[r])):  # number of col in a row
+                turn = self.board[r][c][0]
+                if (turn == 'w' and self.whiteToMove) or (turn == "b" and not self.whiteToMove):
+                    piece = self.board[r][c][1]
+                    if piece == 'p':
+                        self.getPawnMoves(r, c, moves)
+                    elif piece == 'R':
+                        self.getRookMoves(r, c, moves)
+        return moves
+
+    def getPawnMoves(self, r, c, moves):
+        # Has to think about which turn it is.
+        if self.whiteToMove:
+            if self.board[r - 1][c] == "--":
+                moves.append(Move((r, c), (r - 1, c), self.board))
+                if r == 6 and self.board[r - 2][c] == "--":
+                    moves.append(Move((r, c), (r - 2, c), self.board))
+            if c - 1 >= 0:
+                if self.board[r - 1][c - 1][0] == 'b':
+                    moves.append(Move((r, c), (r - 1, c - 1), self.board))
+            if c + 1 <= 7:
+                if self.board[r - 1][c + 1][0] == 'b':
+                    moves.append(Move((r, c), (r - 1, c + 1), self.board))
+
+        if not self.whiteToMove:
+            if self.board[r + 1][c] == "--":
+                moves.append(Move((r, c), (r + 1, c), self.board))
+                if r == 1 and self.board[r + 2][c] == "--":
+                    moves.append(Move((r, c), (r + 2, c), self.board))
+            if c - 1 >= 0:
+                if self.board[r + 1][c - 1][0] == 'w':
+                    print("venstre " + self.board[r + 1][c - 1][0])
+                    moves.append(Move((r, c), (r + 1, c - 1), self.board))
+            if c + 1 <= 7:
+                if self.board[r + 1][c + 1][0] == 'w':
+                    print("højre " + self.board[r + 1][c + 1][0])
+                    moves.append(Move((r, c), (r + 1, c + 1), self.board))
+
+    def getRookMoves(self, r, c, moves):
+        pass
+
+    def undoMove(self):
+        if len(self.moveLog) != 0:
+            move = self.moveLog.pop()
+            self.board[move.startRow][move.startCol] = move.pieceMoved
+            self.board[move.endRow][move.endCol] = move.pieceCaptured
+            self.whiteToMove = not self.whiteToMove  # switches the turn back
 
 
-class Move():
+class Move:
 
     def __init__(self, startSq, endSq, board):
         self.startRow = startSq[0]
@@ -35,3 +90,9 @@ class Move():
         self.endCol = endSq[1]
         self.pieceMoved = board[self.startRow][self.startCol]
         self.pieceCaptured = board[self.endRow][self.endCol]
+        self.moveID = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
+
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.moveID == other.moveID
+        return False
